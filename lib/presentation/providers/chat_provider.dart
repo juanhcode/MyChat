a6db22b2ac5d27bdcd0f825ea35e7ab5821aada2
Juan Hoyos <juanhoyos1347@gmail.com>
2024-02-07 23:46:23 -0500

    scroll

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -3,12 +3,21 @@ import 'package:mychat/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
 
+  final ScrollController chatScrollController = ScrollController();
+
   List<Message> message = [
     Message(text: "Hola", fromWho: FromWho.mine),
   ];
 
   Future<void> sendMessage(String text)  async{
+    if(text.isEmpty) return;
     message.add(Message(text: text, fromWho: FromWho.mine));
     notifyListeners();
+    await moveScrollToBottom();
+  }
+
+  Future<void> moveScrollToBottom() async{
+    await Future.delayed(const Duration(milliseconds: 100));
+    chatScrollController.animateTo(chatScrollController.position.maxScrollExtent, duration: const Duration(milliseconds: 300), curve: Curves.easeOut);
   }
 }
